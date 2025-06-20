c539c0b91516f6c8beb58f1cd99947a3f3902347
KryptonFox <kirieshka0785@gmail.com>
2025-06-20 15:14:03 +0300

    Improve middleware

--- api/src/middlewares.rs
+++ api/src/middlewares.rs
@@ -1,12 +1,12 @@
 use crate::state::AppState;
-use crate::types::UserType;
-use crate::utils::get_user_id_from_cookie;
+use crate::types::{JwtPayload, UserType};
 use actix_web::dev::{ServiceRequest, ServiceResponse};
 use actix_web::middleware::Next;
 use actix_web::{error, web, Error, HttpMessage};
 use awc::body::MessageBody;
 use entity::prelude::User;
 use entity::user;
+use jsonwebtoken::{decode, DecodingKey, Validation};
 use sea_orm::{ColumnTrait, EntityTrait, QueryFilter};
 
 pub async fn auth_middleware(
@@ -20,29 +20,38 @@ pub async fn auth_middleware(
         .await
         .map_err(error::ErrorInternalServerError)?;
 
-    let user_id = get_user_id_from_cookie(req.request(), &state);
+    let user_id = req
+        .cookie("token")
+        .and_then(|cookie| {
+            decode::<JwtPayload>(
+                &cookie.value(),
+                &DecodingKey::from_secret(state.env.secret.as_bytes()),
+                &Validation::default(),
+            )
+            .ok()
+        })
+        .map(|token| token.claims.user_id);
 
     match user_id {
-        None => next.call(req).await,
         Some(user_id) => {
-            let user = User::find()
+            if let Some(user_type) = User::find()
                 .filter(user::Column::Id.eq(user_id))
                 .one(&state.conn)
                 .await
-                .map_err(error::ErrorUnauthorized)?;
-
-            match user {
-                None => next.call(req).await,
-                Some(user) => {
-                    req.extensions_mut().insert(if user.is_admin {
-                        UserType::Admin(user_id)
+                .map_err(error::ErrorInternalServerError)?
+                .map(|user| {
+                    if user.is_admin {
+                        UserType::Admin(user.id)
                     } else {
-                        UserType::User(user_id)
-                    });
-
-                    next.call(req).await
-                }
+                        UserType::User(user.id)
+                    }
+                })
+            {
+                req.extensions_mut().insert(user_type);
             }
+
+            next.call(req).await
         }
+        None => next.call(req).await,
     }
 }
--- api/src/utils/mod.rs
+++ api/src/utils/mod.rs
@@ -1,21 +1,17 @@
-use crate::state::AppState;
-use crate::types::JwtPayload;
-use actix_web::{web, HttpRequest};
-use jsonwebtoken::{decode, DecodingKey, Validation};
 use sha2::{Digest, Sha256};
 
-pub fn get_user_id_from_cookie(req: &HttpRequest, state: &web::Data<AppState>) -> Option<i64> {
-    req.cookie("token")
-        .and_then(|cookie| {
-            decode::<JwtPayload>(
-                &cookie.value(),
-                &DecodingKey::from_secret(state.env.secret.as_bytes()),
-                &Validation::default(),
-            )
-            .ok()
-        })
-        .map(|token| token.claims.user_id)
-}
+// pub fn get_user_id_from_cookie(req: &HttpRequest, state: &web::Data<AppState>) -> Option<i64> {
+//     req.cookie("token")
+//         .and_then(|cookie| {
+//             decode::<JwtPayload>(
+//                 &cookie.value(),
+//                 &DecodingKey::from_secret(state.env.secret.as_bytes()),
+//                 &Validation::default(),
+//             )
+//             .ok()
+//         })
+//         .map(|token| token.claims.user_id)
+// }
 
 /// Return string with hex value of SHA256 hash of value + salt
 pub fn string_hash_sha256(value: impl Into<String>, salt: &str) -> String {
