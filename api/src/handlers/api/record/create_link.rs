998caab7f24e689f1d0620f4575b940e4c52613f
KryptonFox <kirieshka0785@gmail.com>
2026-03-27 14:17:52 +0300

    Change response format on record creation request

--- api/src/handlers/api/record/create_link.rs
+++ api/src/handlers/api/record/create_link.rs
@@ -1,12 +1,23 @@
 use crate::extractors::UserId;
 use crate::services::{NameService, RecordService};
-use actix_web::{error, post, web, HttpResponse};
-use serde::Deserialize;
+use actix_web::{error, post, web, HttpRequest, HttpResponse};
+use chrono::NaiveDateTime;
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Clone)]
 struct CreateLinkPayload {
     url: String,
     name: Option<String>,
+}
+
+#[derive(Serialize)]
+struct CreateLinkResponse {
+    id: String,
+    owner_id: Option<String>,
+    link: String,
+    created_at: NaiveDateTime,
+    is_temp: bool,
+    expires_at: Option<NaiveDateTime>,
 }
 
 #[post("/create_link")]
@@ -15,10 +26,23 @@ pub async fn create_link_handler(
     name_service: web::Data<NameService>,
     payload: web::Json<CreateLinkPayload>,
     UserId(owner_id): UserId,
+    req: HttpRequest,
 ) -> error::Result<HttpResponse> {
     let record = record_service
         .create_link(name_service.clone(), owner_id, &payload.url, &payload.name)
         .await?;
 
-    Ok(HttpResponse::Ok().json(record))
+    let mut link = req.full_url().clone();
+    link.set_path(record.visible_name.as_str());
+
+    let response = CreateLinkResponse {
+        id: record.id.to_string(),
+        owner_id: record.owner_id.map(|id| id.to_string()),
+        link: link.to_string(),
+        created_at: record.created_at,
+        is_temp: record.is_temp,
+        expires_at: record.expires_at,
+    };
+
+    Ok(HttpResponse::Ok().json(response))
 }
--- api/src/handlers/api/record/upload_file.rs
+++ api/src/handlers/api/record/upload_file.rs
@@ -3,7 +3,9 @@ use crate::services::{NameService, RecordService};
 use actix_multipart::form::tempfile::TempFile;
 use actix_multipart::form::text::Text;
 use actix_multipart::form::MultipartForm;
-use actix_web::{error, post, web, HttpResponse};
+use actix_web::{error, post, web, HttpRequest, HttpResponse};
+use chrono::NaiveDateTime;
+use serde::Serialize;
 
 #[derive(Debug, MultipartForm)]
 struct UploadForm {
@@ -12,20 +14,47 @@ struct UploadForm {
     name: Option<Text<String>>,
 }
 
+#[derive(Serialize)]
+struct UploadFileResponse {
+    id: String,
+    owner_id: Option<String>,
+    link: String,
+    cdn_link: String,
+    created_at: NaiveDateTime,
+    hash: Option<String>,
+    mime_type: Option<String>,
+    is_temp: bool,
+    expires_at: Option<NaiveDateTime>,
+}
+
 #[post("/upload")]
 pub async fn upload_file_handler(
     record_service: web::Data<RecordService>,
     name_service: web::Data<NameService>,
     MultipartForm(form): MultipartForm<UploadForm>,
     UserId(owner_id): UserId,
+    req: HttpRequest,
 ) -> error::Result<HttpResponse> {
-    // convert multipart text to string
     let name = form.name.map(|n| n.to_string());
 
-    //
     let record = record_service
         .upload_file(name_service.clone(), owner_id, form.file, &name)
         .await?;
 
-    Ok(HttpResponse::Ok().json(record))
+    let mut link = req.full_url().clone();
+    link.set_path(record.visible_name.as_str());
+
+    let response = UploadFileResponse {
+        id: record.id.to_string(),
+        owner_id: record.owner_id.map(|id| id.to_string()),
+        link: link.to_string(),
+        cdn_link: record.url,
+        hash: record.hash,
+        mime_type: record.mime_type,
+        created_at: record.created_at,
+        is_temp: record.is_temp,
+        expires_at: record.expires_at,
+    };
+
+    Ok(HttpResponse::Ok().json(response))
 }
