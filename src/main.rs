4bcf8f7e02b377fe90ab666dac6cc9bfe87b0859
KryptonFox <kirieshka0785@gmail.com>
2026-03-27 14:18:35 +0300

    Change year

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
     println!("[krfx-rust] Starting krfx.ru API v3 Rust Edition");
-    println!("[krfx-rust] KryptonFox (c) 2025");
+    println!("[krfx-rust] KryptonFox (c) 2026");
     api::main();
 }
